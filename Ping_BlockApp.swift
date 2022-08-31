33a32ded72bfcc82a09eadb3a146b0db184c46c7
amaasi26 <112572461+amaasi26@users.noreply.github.com>
2022-09-01 01:29:48 +0900

    Add files via upload

---
+++ Ping_BlockApp.swift
@@ -0,0 +1,17 @@
+//
+//  Ping_BlockApp.swift
+//  Ping Block
+//
+//  Created by Amaan Singh on 2022/01/30.
+//
+
+import SwiftUI
+
+@main
+struct Ping_BlockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
